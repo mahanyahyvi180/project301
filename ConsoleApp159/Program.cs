0c2f5f113780a0eab7985736ef7aebb1f10058ec
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2024-01-30 22:18:40 +0330

    Add files via upload

---
+++ ConsoleApp159/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var buffer1 = new StringBuilder();
+            var buffer2 = new StringBuilder();
+            var buffer3 = new StringBuilder("hello");
+
+            Console.WriteLine($"buffer1=\"{buffer1}\"");
+            Console.WriteLine($"buffer2=\"{buffer2}\"");
+            Console.WriteLine($"buffer3=\"{buffer3}\"");
+
+            Console.ReadLine();
+        }
+    }
+}
